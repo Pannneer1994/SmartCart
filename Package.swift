38220992dae719af7f19db2ad544803a2f2d7959
Panneer selvam <selvamjan2@gmail.com>
2025-05-20 16:58:05 +0530

    Update version

--- Package.swift
+++ Package.swift
@@ -11,20 +11,18 @@ let package = Package(
     products: [
         .library(
             name: "SmartCart",
-            targets: ["SmartCart"]
-        )
+            targets: ["SmartCart"]),
     ],
     
     // MARK: - Targets
     // Targets define modules or test suites. They may depend on each other or on external products.
     targets: [
-        
-        // MARK: Binary Dependency (SDWebImage)
-        // This binary target integrates SDWebImage as a dynamic xcframework.
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "SDWebImage",
-            url: "https://github.com/SDWebImage/SDWebImage/releases/download/5.21.0/SDWebImage-dynamic.xcframework.zip",
-            checksum: "e034ea04f5e86866bc3081d009941bd5b2a2ed705b3a06336656484514116638"
+            url: "https://github.com/SDWebImage/SDWebImage/releases/download/5.20.0/SDWebImage-dynamic.xcframework.zip",
+            checksum: "b5e9ed9a177eb3d5a1f8890ee7ded39d8e92ecd2d0e423223ce53adcaeb01615"
         ),
         
         // MARK: Main Target - SmartCart
@@ -43,8 +41,7 @@ let package = Package(
         // MARK: Test Target - Unit Tests
         .testTarget(
             name: "SmartCartTests",
-            dependencies: ["SmartCart"]
-        )
+            dependencies: ["SmartCart"]),
     ]
 )
 
