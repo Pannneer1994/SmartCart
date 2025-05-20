9fe894cbb089be66e3166c75589a01d44ba916c6
Panneer selvam <selvamjan2@gmail.com>
2025-05-20 17:35:31 +0530

    Update version

--- Package.swift
+++ Package.swift
@@ -1,47 +1,33 @@
-// swift-tools-version:5.9
-// This Swift package requires Swift 5.9 or later.
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SmartCart",
-    
-    // MARK: - Products
-    // This package exposes a library called "SmartCart" for external use.
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SmartCart",
             targets: ["SmartCart"]),
     ],
-    
-    // MARK: - Targets
-    // Targets define modules or test suites. They may depend on each other or on external products.
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "SDWebImage",
-            url: "https://github.com/SDWebImage/SDWebImage/releases/download/5.20.0/SDWebImage-dynamic.xcframework.zip",
-            checksum: "b5e9ed9a177eb3d5a1f8890ee7ded39d8e92ecd2d0e423223ce53adcaeb01615"
+            url: "https://github.com/SDWebImage/SDWebImage/releases/download/5.21.0/SDWebImage-dynamic.xcframework.zip",
+            checksum: "e034ea04f5e86866bc3081d009941bd5b2a2ed705b3a06336656484514116638"
         ),
-        
-        // MARK: Main Target - SmartCart
         .target(
             name: "SmartCart",
-            
-            // Declaring dependency on the binary SDWebImage target.
             dependencies: ["SDWebImage"],
-            
-            // Process resource files located in the Resources folder.
             resources: [
                 .process("Resources")
             ]
         ),
-        
-        // MARK: Test Target - Unit Tests
         .testTarget(
             name: "SmartCartTests",
             dependencies: ["SmartCart"]),
     ]
 )
-
